e88f6f8d244e263f3e5d368b3ee8088c24cf45aa
philhoward23 <phil.howard@gmail.com>
2018-12-01 19:29:29 +0000

    add count of repeats needed

--- day1/day1soln.py
+++ day1/day1soln.py
@@ -1,14 +1,18 @@
+#part 1
+#sum signed integers
 day1input=open("input")
 input = day1input.readlines()
 sum(int(x.strip()) for x in input)
 
-
 #part 2
-freqs=set()
+#repeatedly sum a list of signed integers until the sum is equal to a previous value for the first time
+freqs=set([0])
 cumsum=0
 dup=False
+repeatCount=0
 
 while not dup:
+	print repeatCount
 	for x in input:
 		cumsum=cumsum+int(x.strip())
 		if cumsum in freqs:
@@ -17,4 +21,4 @@ while not dup:
    		 	break
   		else:
 			freqs.add(cumsum)
-		
+	repeatCount+=1
